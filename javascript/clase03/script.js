2bd1db424d06913f550aade5714780481b9f2d58
davidfernandez83 <94862407+davidfernandez83@users.noreply.github.com>
2022-01-31 16:47:41 -0300

    js primera entrega
    ciclos

---
+++ javascript/clase03/script.js
@@ -0,0 +1,6 @@
+let dato = prompt("Por favor ingrese un número a continuación:");
+
+console.log("El número que se igresó se multiplica a continuacion por 2, 3 y 4");
+for(let i = 2; i <= 4; i++){
+    console.log(dato*i);
+}
